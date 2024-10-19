0ea052aa444c865614205c0649aee0ec8ff0b467
deveshbajaj123 <125900558+deveshbajaj123@users.noreply.github.com>
2024-10-20 03:36:13 +0530

    Update Hashtable.cpp

--- Hashtable.cpp
+++ Hashtable.cpp
@@ -107,26 +107,3 @@ public:
     }
 };
 
-int main() {
-    int initialSize = 7; 
-    HashTable ht(initialSize);
-
-    
-    ht.insert(1);
-    ht.printTable();
-    ht.insert(6);
-    ht.printTable();
-    ht.insert(15);
-    ht.printTable(); 
-    ht.insert(25);
-    ht.printTable();
-    ht.remove(15);
-    ht.printTable();
-    ht.insert(29);  
-    ht.printTable(); 
-
-    int find = ht.search(22);
-    std::cout << "Found at: " << find << std::endl;
-
-    return 0;
-}
